273c9f953053822e697ff1ec52c0b8560ef1e7d2
vagrant <vagrant@rails-dev-box>
2014-10-20 20:04:28 +0000

    Associated and nested topics and posts.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
+    belongs_to :topic
   scope :created_before, ->(time) { where("created_at < ?", time) }
   scope :ordered_by_title, -> { order('title DESC') }
   scope :ordered_by_reverse_created_at, -> {order('created_at ASC')}
