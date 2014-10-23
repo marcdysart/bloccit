5412af86cc1fcebb14d55c335f8cc8aa1d200d4e
Marc Dysart <dysart.marc@gmail.com>
2014-10-23 18:41:18 +0000

    Created a different handmade solution where form_group returns @error_class to add has-error class to html when there is an error present

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,22 @@
-module ApplicationHelper
-  def my_name
-      :'Marc Dysart'
-  end
-def form_group_tag(errors, &block)
+ module ApplicationHelper
+   def form_group_tag(errors, &block)
      if errors.any?
        content_tag :div, capture(&block), class: 'form-group has-error'
      else
        content_tag :div, capture(&block), class: 'form-group'
      end
    end
-end
 
-def form_group(errors)
-     if errors.any?
-      @error_class='has-error'
-     else
-       @error_class=''
-     end
+   def form_group(errors)
+        if errors.any?
+         @error_class='has-error'
+        else
+         @error_class=''
+        end
    end
-end
+
+ end
+
 
 
 def markdown(text)
@@ -26,4 +24,8 @@ def markdown(text)
   extensions = {fenced_code_blocks: true}
   redcarpet = Redcarpet::Markdown.new(renderer, extensions)
   (redcarpet.render text).html_safe
+end
+
+def my_name
+    :'Marc Dysart'
 end
