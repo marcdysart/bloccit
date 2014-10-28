b32e597846283fa11f7b02f322e748d99e6836ab
Marc Dysart <dysart.marc@gmail.com>
2014-10-28 18:43:14 +0000

    Change views to show comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -21,17 +21,25 @@ class CommentsController < ApplicationController
 
   def create
     @topic = Topic.find(params[:topic_id])
-    @post = Post.find(params[:id])
-    @comment = current_user.comments.build(post_params)
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(comment_params)
+    # @comment = current_user.comments.build(comment_params)
+    @comment.post = @post
     @comment.post.topic = @topic
     authorize @comment
 
     if @comment.save
       flash[:notice] = "Comment was saved."
-      redirect_to [@topic, @post, @comment]
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the comment. Please try again."
       render :new
     end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'comments/index'
-
-  get 'comments/new'
-
-  get 'comments/show'
-
-  get 'comments/edit'
-
-  get 'comments/create'
-
-  devise_for :users
+    devise_for :users
     resources :users, only: [:update]
-   resources :topics do
-     resources :posts, except: [:index]
-     resources :posts do
-       resources :comments, only: [:create]
-     end
-   end
+    resources :topics do
+         resources :posts, except: [:index] do
+           resources :comments, only: [:create]
+         end
+    end
 
 
 
