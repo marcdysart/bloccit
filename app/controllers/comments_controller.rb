66f9c833207538f30f2f6823fe296fae73cb61db
Marc Dysart <dysart.marc@gmail.com>
2014-10-30 02:29:17 +0000

    Change route template

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,10 @@ class CommentsController < ApplicationController
 
   def destroy
      @topic = Topic.find(params[:topic_id])
-     @post = @topic.posts.find(params[:post_id])
+     @post = Post.find(params[:post_id])
      @comment = @post.comments.find(params[:id])
-
      authorize @comment
+
      if @comment.destroy
        flash[:notice] = "Comment was removed."
        redirect_to [@topic, @post]
