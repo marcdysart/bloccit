a34cbf212393a1d91de111a63c5886a0783ee21c
Marc Dysart <dysart.marc@gmail.com>
2014-11-04 16:37:55 +0000

    Added down_vote method to Rspec and made the test code more dry

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -4,17 +4,46 @@
 
    include TestFactories
    include Devise::TestHelpers
+   before do
+     request.env["HTTP_REFERER"] = '/'
+     @user = authenticated_user
+     @post = associated_post
+     sign_in @user
+   end
 
    describe '#up_vote' do
      it "adds an up-vote to the post" do
-       request.env["HTTP_REFERER"] = '/'
-       @user = authenticated_user
-       @post = associated_post
-       sign_in @user
-
        expect {
          post( :up_vote, post_id: @post.id )
        }.to change{ @post.up_votes }.by 1
      end
    end
+
+
+   describe '#down_vote' do
+     it "adds an down-vote to the post" do
+       expect {
+         post( :down_vote, post_id: @post.id )
+       }.to change{ @post.down_votes }.by 1
+     end
+   end
+
  end
+
+def associated_post(options={})
+   post_options = {
+     title: 'Post title',
+     body: 'Post bodies must be pretty long.',
+     topic: Topic.create(name: 'Topic name'),
+     user: authenticated_user
+   }.merge(options)
+  Post.create(post_options)
+end
+
+def authenticated_user(options={})
+   user_options = {email: "email#{rand}@fake.com", password: 'password'}.merge(options)
+   user = User.new(user_options)
+   user.skip_confirmation!
+   user.save
+   user
+end
