c501f2c0bce364125afe28b2ad819493484aaa50
Marc Dysart <dysart.marc@gmail.com>
2014-11-05 19:13:36 +0000

    Add Rspec test of favorited(post)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,20 @@ require 'rails_helper'
    include TestFactories
 
    describe "#favorited(post)" do
-     xit "returns `nil` if the user has not favorited the post" do
+
+     before do
+        @post = associated_post
+        @user = authenticated_user
      end
 
-     xit "returns the appropriate favorite if it exists" do
+     it "returns `nil` if the user has not favorited the post" do
+       expect( @user.favorites.find_by_post_id(@post.id) ).to be_nil
+
+     end
+
+     it "returns the appropriate favorite if it exists" do
+       favorite = @user.favorites.where(post: @post).create
+       expect( @user.favorites.find_by_post_id(@post.id).class ).to eq(Favorite)
      end
    end
  end
