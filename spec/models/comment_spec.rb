c0fb8fb718fe0138c85f28323fd75d05272b0eac
Marc Dysart <dysart.marc@gmail.com>
2014-11-05 23:45:37 +0000

    Added redirect to topics after signing in, Change opacity of arrows after voting, check permission to send emails

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -12,6 +12,10 @@ require 'rails_helper'
        @comment = Comment.new(body: 'My comment', post: @post, user_id: 10000)
      end
 
+     # We don't need to change anything for this condition;
+     # The email_favorites attribute defaults to true
+     context "with user's permission" do
+
      it "sends an email to users who have favorited the post" do
        @user.favorites.where(post: @post).create
 
@@ -26,8 +30,23 @@ require 'rails_helper'
      it "does not send emails to users who haven't" do
        expect( FavoriteMailer )
          .not_to receive(:new_comment)
-
        @comment.save
      end
+   end
+
+     context "without permission" do
+
+       before { @user.update_attribute(:email_favorites, false) }
+
+       it "does not send emails, even to users who have favorited" do
+         @user.favorites.where(post: @post).create
+
+         expect( FavoriteMailer )
+           .not_to receive(:new_comment)
+
+         @comment.save
+       end
+     end
+
    end
  end
